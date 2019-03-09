ae08f11725c3519e73637a67d5cbe7cdaa200652
Eric Chen <ericthechen@gmail.com>
2019-03-09 10:12:03 -0800

    removing styles

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import './style.css';
-
-function Nav({ children }) {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary rounded">
-      <div>
-        <div>
-          <a className="navbar-brand" href="/">
-            {children}
-          </a>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
-export default Nav;
+export default "Nav.js";
