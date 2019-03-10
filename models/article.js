2bdd95fc081260096fa363ce20fa6744e5b57afa
Matt Zuckermann <mczuckermann@gmail.com>
2019-03-09 17:08:35 -0800

    Naming change of dbModel Export

--- models/article.js
+++ models/article.js
@@ -1,14 +1,12 @@
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const mongoose = require('mongoose');
 
 const articleSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
   saved: { type: Boolean, default: false },
-  summary: { type: String, required: false },
-  date: { type: Date, required: true },
+  summary: {type: String, required: false},
+  date: { type: Date, required: true }
 });
 
-const Article = mongoose.model('Article', articleSchema);
-
-export default Article;
+const Article = module.exports = mongoose.model("Article", articleSchema);
