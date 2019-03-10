01af6f37c9f233a831bd34f4f09c010cdec4a46f
Matt Zuckermann <mczuckermann@gmail.com>
2019-03-09 16:38:36 -0800

    Additional reformatting to server

--- models/article.js
+++ models/article.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
 const articleSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
   saved: { type: Boolean, default: false },
-  summary: {type: String, required: false},
-  date: { type: Date, required: true }
+  summary: { type: String, required: false },
+  date: { type: Date, required: true },
 });
 
-const Article = module.exports = mongoose.model("Article", articleSchema);
+const Article = mongoose.model('Article', articleSchema);
+
+export default Article;
