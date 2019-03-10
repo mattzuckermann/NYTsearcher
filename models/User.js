d209d2694747312ae8c64d90c2133f1252f11d5f
Matt Zuckermann <mczuckermann@gmail.com>
2019-03-09 17:49:08 -0800

    Added User Schema for use in login

--- models/User.js
+++ models/User.js
@@ -1,8 +1,48 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+// eslint-disable-next-line prefer-destructuring
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  id: { type: String, required: true }
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const User = module.exports = mongoose.model("User", userSchema);
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
