17d4e4fb77cbd8b4c20bb44ae818adf2cae9bc2e
Wyatt C <cochranwyatt@yahoo.com>
2019-03-13 18:43:34 -0700

    commit for pull

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
 
-const Nav = () =>
-
-  <nav className="navbar navbar-dark bg-primary" style={{position: 'fixed', zIndex: 100, width: '100%'}}>
-    <Link className="navbar-brand" to="/">NYT Article Search</Link>
-    <ul className="navbar-nav" style={{float: 'right'}}>
+const Nav = () => (
+  <nav className="navbar navbar-dark" style={{ position: 'fixed', zIndex: 100, width: '100%' }}>
+    <Link className="navbar-brand" to="/">
+      NYT Best Seller Search
+    </Link>
+    <ul className="navbar-nav" style={{ float: 'right' }}>
       <li className="nav-item">
-        <Link to="/"><button type="button" className="btn btn-info">Home</button></Link>
+        <Link to="/">
+          <button type="button" id="btn1" className="btn">
+            Home
+          </button>
+        </Link>
       </li>
       <li className="nav-item">
-        <Link to="/savedArticles"><button type="button" className="btn btn-info">Saved Articles</button></Link>
+        <Link to="/savedArticles">
+          <button type="button" id="btn2" className="btn">
+            Saved Articles
+          </button>
+        </Link>
       </li>
     </ul>
-</nav>;
+  </nav>
+);
 
 export default Nav;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
-const articleRoutes = require("./articles");
+const router = require('express').Router();
+const articleRoutes = require('./articles');
 
 // Article routes
-router.use("/articles", articleRoutes);
+router.use('/articles', articleRoutes);
 
 module.exports = router;
