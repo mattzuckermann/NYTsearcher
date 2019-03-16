abcac3a5f0245f48e78a483eb1d1f345c7feaf07
Case <38591271+saladshootrdlux@users.noreply.github.com>
2019-03-16 11:27:58 -0700

    accepted conflicts

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const articleRoutes = require('./articles');
-
-// Article routes
-router.use('/articles', articleRoutes);
-
-=======
 const router = require("express").Router();
 const articleRoutes = require("./articles");
 const recommendationRoutes = require("./recommendations");
 // Article routes
 router.use("/articles", articleRoutes);
 router.use("/recommendations", recommendationRoutes);
->>>>>>> master
 module.exports = router;
