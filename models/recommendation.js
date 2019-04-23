3c514bea87bfab6c9d6c4ddc464928c82b4d916c
Matt Zuckermann <mczuckermann@gmail.com>
2019-04-22 21:33:47 -0700

    Refactored code

--- models/recommendation.js
+++ models/recommendation.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recSchema = new Schema({
-    sender: {type: String, required: false},
-    receiver:{type: String, required: false},
-    message: {type: String, required: false},
-    title : {type: String, required: false},
-    url : {type: String, required: false}
+  sender: { type: String, required: false },
+  receiver: { type: String, required: false },
+  message: { type: String, required: false },
+  title: { type: String, required: false },
+  url: { type: String, required: false },
 });
 
-const Recommendation = module.exports = mongoose.model("Recommendation", recSchema);
+const Recommendation = (module.exports = mongoose.model('Recommendation', recSchema));
