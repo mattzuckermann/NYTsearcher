43fb2cdc0508286c2634acd7ba3764a5f1d68e42
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-06 21:20:41 -0700

    Trying removing recomendations from index routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const articleRoutes = require('./articles');
-const recommendationRoutes = require('./recommendations');
+// const recommendationRoutes = require('./recommendations');
 const commentRoutes = require('./comments');
 const authenticationRoutes = require('./authentication');
 
 // Matches with "/api"
 router.use('/articles', articleRoutes);
-router.use('/recommendations', recommendationRoutes);
+// router.use('/recommendations', recommendationRoutes);
 router.use('/comments', commentRoutes);
 router.use('/authentication', authenticationRoutes);
 module.exports = router;
