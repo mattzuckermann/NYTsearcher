b590620dfb1d3722a8b894f59761760f14fb4ca5
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-06 21:17:33 -0700

    Trying removing recomendations

--- routes/api/recommendations.js
+++ routes/api/recommendations.js
@@ -1,15 +1,12 @@
-const router = require("express").Router();
-const rC = require("../../controllers/recommendationController");
+// const router = require("express").Router();
+// const rC = require("../../controllers/recommendationController");
 
+// // Matches with "/api/recommendations"
+// router.route("/").post( rC.createRecommendation);
 
-// Matches with "/api/recommendations"
-router.route("/").post( rC.createRecommendation);
+// router.route("/user/:user").get(rC.getUser);
 
-router.route("/user/:user").get(rC.getUser);
+// // Matches with "/api/recommendations/:id"
+// router.route("/:user").get(rC.getRecommendation);
 
-// Matches with "/api/recommendations/:id"
-router.route("/:user").get(rC.getRecommendation);
-
-
-
-module.exports = router;
+// module.exports = router;
